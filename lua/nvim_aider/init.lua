a9a707b24f2987b3c73a3589f02c838a572298a4
Georges <georges.alkhouri@gmail.com>
2025-04-08 21:46:23 +0200

    refactor(api): deprecate direct terminal access (#42)

--- lua/nvim_aider/init.lua
+++ lua/nvim_aider/init.lua
@@ -1,8 +1,26 @@
 local M = {}
 
 M.config = require("nvim_aider.config")
-M.terminal = require("nvim_aider.terminal")
 M.api = require("nvim_aider.api")
+
+local deprecation_shown = false
+
+setmetatable(M, {
+  __index = function(tbl, key)
+    if key == "terminal" then
+      if not deprecation_shown then
+        vim.notify(
+          "[nvim_aider] 'nvim_aider.terminal' is deprecated and will be removed in a future release. Please use 'nvim_aider.api' instead.",
+          vim.log.levels.WARN
+        )
+        deprecation_shown = true
+      end
+      return require("nvim_aider.terminal")
+    end
+
+    return rawget(tbl, key)
+  end,
+})
 
 ---@param opts? nvim_aider.Config
 function M.setup(opts)
